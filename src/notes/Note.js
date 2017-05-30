0fa83a450ebabd6f547ba2fed0952fbfb3d01ebb
peteryunkim <peteryunkim@gmail.com>
2017-05-30 13:06:01 -0400

    fixed proper note list rendering

--- src/notes/Note.js
+++ src/notes/Note.js
@@ -12,12 +12,11 @@ class Note extends React.Component {
 
 
 	render(){
-		console.log(this.props)
 		return(
 			<div className="note card">
 				<div className="note-body">
 					<div className="note-text">
-						{ this.state.note.body }
+						{ this.props.note.body }
 						<button onClick={() => this.props.onEdit(this.state.note.id)}>Edit</button>
 						<button onClick={() => this.props.onDelete(this.state.note.id)}>Delete</button>
 					</div>
